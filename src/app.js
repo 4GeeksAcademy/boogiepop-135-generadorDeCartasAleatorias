3ec6dba148ff6c5a8124038671d362048407d596
boogiepop-135 <levi.eduardo2024@gmail.com>
2025-02-11 03:11:41 +0000

    trabajo porgresado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  //haber que cartas tiene el juego de cartas: treboles, diamantes, corazones y picas y van del 2 al 10 y empiezan con as y terminan con jota, reina y rey.  ♦ ♥ ♠ ♣
+let cardNumber=["As",2,3,4,5,6,7,8,9,10,"J","Q","K"]
+let randomCardNumber= cardNumber[Math.floor(Math.random()*cardNumber.length)];
+let tipo=["♦", "♥", "♠", "♣"];
+let randomTipo= tipo[Math.floor(Math.random()*tipo.length)];
+
+console.log(randomCardNumber);
+console.log(randomTipo);
+
+
+document.getElementById("simboloArriba").innerHTML = randomTipo;
+document.getElementById("numeroCentro").innerHTML = randomCardNumber;
+document.getElementById("simboloAbajo").innerHTML = randomTipo;
+
 };
